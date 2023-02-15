123d234948fc84f99e6675ab83bca8e2d9b42377
Matt Nafarrete <mattnaf@gmail.com>
2023-02-15 11:26:51 -0800

    big changes

---
+++ components/PaymentModal.js
@@ -0,0 +1,2 @@
+import React, { useState } from "react";
+import styles from '@/styles/Home.module.css'
---
+++ components/ViewCartModal.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import styles from '@/styles/Home.module.css'
+
+const ViewCartModal = (props) => {
+    
+}
+
+export default ViewCartModal
