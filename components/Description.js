d7c5832cc9c54b33f95f835cf537d983ab502522
Matt Nafarrete <mattnaf@gmail.com>
2023-02-17 14:21:38 -0800

    quoteFix

--- components/Description.js
+++ components/Description.js
@@ -5,7 +5,7 @@ const Description = (props) => {
 
     return (
         <div className={styles.descriptionContainer}>
-            <p>Welcome to the "Kitchen Sync" demo page. On this screen you see two QR codes, each one representing a different table. Imagine they are on a table at a restaurant and scanning them with your phone gives you access to the menu and the ability to place orders. Once orders are placed they will be displayed on the table below as if they were presented to kitchen staff to show the incoming orders. When the orders are "complete", you can double tap them to "complete" them.</p>
+            <p>Welcome to the "Kitchen Sync" demo page. On this screen you see two QR codes, each one representing a different table. Imagine they are on a table at a restaurant and scanning them with your phone gives you access to the menu and the ability to place orders. Once orders are placed they will be displayed on the table below as if they were presented to kitchen staff to show the incoming orders. When the orders are &quot;complete&quot;, you can double tap them to &quot;complete&quot; them.</p>
         </div>
     )
 }
