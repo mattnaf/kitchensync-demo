bb90d0a1ab47dde66d1ddac02c66ff924d76bed9
Matt Nafarrete <mattnaf@gmail.com>
2023-02-17 15:39:32 -0800

    finalCleanUp

--- pages/menu/[tableNumber].js
+++ pages/menu/[tableNumber].js
@@ -44,7 +44,7 @@ const Menu = (props) => {
         price: 29.99,
         description: "A juicy and flavorful chicken breast grilled to perfection with lemon and herbs.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image:"https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/lemonChicken.png?alt=media&token=4d4399d2-03a7-4248-a771-0b314313e67b"
       },
       {
         id:2,
@@ -52,7 +52,7 @@ const Menu = (props) => {
         price: 12.99,
         description: "Fresh mozzarella, ripe tomatoes, and basil leaves drizzled with balsamic reduction.",
         type: "appetizer",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/capreseSalad.png?alt=media&token=69914394-cfb3-45fd-b3f5-b6418370c293"
       },
       {
         id:3,
@@ -60,7 +60,7 @@ const Menu = (props) => {
         price: 18.99,
         description: "Creamy macaroni and cheese baked to bubbly perfection.",
         type: "side",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/bakedMac.png?alt=media&token=53c7eb7d-87cb-4082-a67b-88c0082feae9"
       },
       {
         id:4,
@@ -68,7 +68,7 @@ const Menu = (props) => {
         price: 7.99,
         description: "A classic soup made with ripe tomatoes, cream, and spices.",
         type: "appetizer",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/tomatoeSoup.png?alt=media&token=79be5bd7-419e-46d7-83e7-76fc38a26e6d"
       },
       {
         id:5,
@@ -76,7 +76,7 @@ const Menu = (props) => {
         price: 34.99,
         description: "A juicy ribeye steak served with crispy french fries.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/steakFrites.png?alt=media&token=e8adbd2d-7e6b-4896-9b2c-3cbc3e8d6178"
       },
       {
         id:6,
@@ -84,7 +84,7 @@ const Menu = (props) => {
         price: 19.99,
         description: "Penne pasta tossed with basil pesto and sun-dried tomatoes.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/pestoPasta.png?alt=media&token=10a393dc-f3f0-45cd-b566-ac167862ad8d"
       },
       {
         id:7,
@@ -92,7 +92,7 @@ const Menu = (props) => {
         price: 9.99,
         description: "Creamy mashed potatoes seasoned with butter, milk, and garlic.",
         type: "side",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/mashedPotatoes.png?alt=media&token=f7ad3f54-abe6-47a4-bc80-04588faa90d8"
       },
       {
         id:8,
@@ -100,7 +100,7 @@ const Menu = (props) => {
         price: 22.99,
         description: "A classic Margarita pizza with tomato sauce, mozzarella cheese, and fresh basil.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/margaritaPizza.png?alt=media&token=b5348cfb-200e-4e01-8b99-ab6d3decc663"
       },
       {
         id:9,
@@ -108,7 +108,7 @@ const Menu = (props) => {
         price: 8.99,
         description: "Crisp romaine lettuce, croutons, and parmesan cheese dressed with classic Caesar dressing.",
         type: "appetizer",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/cesarSalad.png?alt=media&token=b27e038e-8106-4777-be89-6f68f081f60a"
       },
       {
         id:10,
@@ -116,7 +116,7 @@ const Menu = (props) => {
         price: 12.99,
         description: "A medley of seasonal vegetables grilled to perfection.",
         type: "side",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/grilledVeggies.png?alt=media&token=1d2ce6e2-a434-4b34-9bb5-9bc73d274d89"
       },
       {
         id:11,
@@ -124,7 +124,7 @@ const Menu = (props) => {
         price: 14.99,
         description: "Stir-fried rice with vegetables and eggs.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/friedRice.png?alt=media&token=5551ee7c-9642-4a6b-bb97-ba25812482c8"
       },
       {
         id:12,
@@ -132,7 +132,7 @@ const Menu = (props) => {
         price: 15.99,
         description: "A classic cobb salad with romaine lettuce, grilled chicken, bacon, eggs, and blue cheese.",
         type: "entree",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/cobbSalad.png?alt=media&token=ba80a053-84f1-4b0d-b013-c79716609798"
       },
       {
         id:13,
@@ -140,7 +140,7 @@ const Menu = (props) => {
         price: 24.99,
         description: "Juicy crab cakes seasoned with herbs and spices, pan-fried to crispy perfection.",
         type: "appetizer",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/crabCakes.png?alt=media&token=d2726a0d-5d25-4e4a-92e3-7449fed669d9"
       },
       {
         id:14,
@@ -148,7 +148,7 @@ const Menu = (props) => {
         price: 3.50,
         description: "Refreshing glass of lemonade made with real lemons.",
         type: "drink",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/lemondae.png?alt=media&token=3a12ed4f-7493-4003-9e80-695d53800cf2"
       },
       {
         id:15,
@@ -156,7 +156,7 @@ const Menu = (props) => {
         price: 2.50,
         description: "Brewed and chilled iced tea with a hint of lemon.",
         type: "drink",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/icedTea.png?alt=media&token=43498d50-e4c3-41c2-a0f9-9e04751c688c"
       },
       {
         id:16,
@@ -164,7 +164,7 @@ const Menu = (props) => {
         price: 2.75,
         description: "A classic soda with your choice of flavor.",
         type: "drink",
-        image: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80"
+        image: "https://firebasestorage.googleapis.com/v0/b/kitchensync-fd489.appspot.com/o/soda.png?alt=media&token=13a6ec9e-5def-4b6f-889d-344d1a28c3e9"
       }
   ];
 
